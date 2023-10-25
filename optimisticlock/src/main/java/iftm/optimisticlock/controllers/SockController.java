21bb63f72170b770591bb3911bcac4222796d1db
devshinon-br <rezendekeyla126@gmail.com>
2023-10-24 22:52:17 -0300

    Create endpoint responsible for update quantity

--- optimisticlock/src/main/java/iftm/optimisticlock/controllers/SockController.java
+++ optimisticlock/src/main/java/iftm/optimisticlock/controllers/SockController.java
@@ -4,13 +4,11 @@ import iftm.optimisticlock.models.SockItem;
 import iftm.optimisticlock.models.dto.SockItemDto;
 import iftm.optimisticlock.repositories.SockItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sock")
@@ -28,6 +26,31 @@ public class SockController {
         repository.save(item);
 
         return ResponseEntity.ok("Successfully created quantity for new item with ID " + item.getId());
+    }
+
+    @PostMapping("/item/{id}/update")
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public ResponseEntity<?> updateSockItemQuantity(
+        @PathVariable Long id,
+        @RequestParam int incrementAmount) {
+
+        final SockItem item = repository.findById(id);
+
+        if (item == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item not found with ID " + id);
+        }
+
+        int currentQuantity = item.getQuantity();
+        int updatedQuantity = incrementAmount + currentQuantity;
+
+        if (updatedQuantity < 0) {
+            return ResponseEntity.badRequest().body("Updated quantity cannot be negative");
+        }
+
+        item.setQuantity(updatedQuantity);
+        repository.save(item);
+
+        return ResponseEntity.ok("Successfully updated quantity for item with ID " + id + " to " + updatedQuantity);
     }
 
 }
