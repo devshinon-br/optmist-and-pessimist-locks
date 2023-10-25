47dc6616ed983eb12d100a1b823ab678503009b4
devshinon-br <rezendekeyla126@gmail.com>
2023-10-24 22:43:38 -0300

    second commit

---
+++ optimisticlock/src/test/java/iftm/optimisticlock/OptimisticlockApplicationTests.java
@@ -0,0 +1,13 @@
+package iftm.optimisticlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimisticlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
